15029005ac49777a9229b3aa22dfd3c488964055
Vedant <your-vedantta39@gmail.com>
2026-09-02 23:25:56 +0530

    Prepare PingNest for deployment

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
   },
 });
 
@@ -27,6 +28,7 @@ io.on("connection", (socket) => {
   }
 
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
 
