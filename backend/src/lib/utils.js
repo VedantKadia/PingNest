ce4cf57fcdc7d936a7f38913172c32cbf89b6bdf
Vedant <your-vedantta39@gmail.com>
2026-09-03 11:47:58 +0530

    Fix production authentication cookie

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -12,8 +12,8 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "lax",
-    secure: false,
+    secure: true,
+    sameSite: "none",
   });
 
   return token;
